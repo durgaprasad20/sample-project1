849a1c2211392e6f530e961b15ebfdbf0fbe182a
durgaprasad20 <61267327+durgaprasad20@users.noreply.github.com>
2021-04-08 10:43:49 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,4 @@
 provider "azurerm" {
   features {}
-  subscription_id = "f70de31c-f9a7-4461-87bd-3a87efaaf3f3"
-  client_id       = "4e6c799d-316f-4f4c-bc19-903d785b3db1"
-  client_secret   = "XP~4zdgvV3.polRgR5_1LVGzJk017_b_Rb"
-  tenant_id       = "e3a29ccb-e13d-41c9-8c72-3ab468c95172"
-
 }
 
